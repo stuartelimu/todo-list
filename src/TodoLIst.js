3a7f75e37c8ea49885ed3ab3e858746fc864ca03
ElimuStuart <stuartelimu@gmail.com>
2020-02-24 22:12:25 +0300

    add create functionality

---
+++ src/TodoLIst.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: { text: "", key: "" }
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  componentDidUpdate() {
+    this._inputElement.focus();
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      console.log(newItem);
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {text: "", key: ""}
+      })
+    }
+  }
+
+  handleInput(e) {
+    const itemText = e.target.value;
+    const currentItem = {text: itemText, key: Date.now()}
+    this.setState({
+      currentItem,
+    });
+  }
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              placeholder="Task"
+              ref={a => (this._inputElement = a)}
+              onChange={this.handleInput}
+              value={this.state.currentItem.text}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
